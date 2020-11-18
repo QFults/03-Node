87d9a1f892c6ab831ad91ea7be73b69eecdeff4b
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-18 14:23:22 -0800

    code

--- app.js
+++ app.js
@@ -18,9 +18,22 @@
 
 // user.printInfo()
 
-const User = (name, email, phone, address) => ({ name, email, phone, address })
+// const User = (name, email, phone, address) => ({ name, email, phone, address })
 
-let user = User('John Doe', 'johndoe@gmail.com', 1234567890, '123 Main St')
-let user2 = User('Jane Doe', 'janedoe@gmail.com', 0987654321, '321 South St')
+// let user = User('John Doe', 'johndoe@gmail.com', 1234567890, '123 Main St')
+// let user2 = User('Jane Doe', 'janedoe@gmail.com', 0987654321, '321 South St')
+
+// console.log(user)
+
+class User {
+  constructor (name, email, phone, address) {
+    this.name = name
+    this.email = email
+    this.phone = phone
+    this.address = address
+  }
+}
+
+let user = new User('John Doe', 'johndoe@gmail.com', 1234567890, '123 Main St')
 
 console.log(user)
