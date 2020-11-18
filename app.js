65d4047d0ba2c072a73e846566c7548378ce540e
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-18 14:07:31 -0800

    code

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 
-function User (name, email, phone, address) {
-  this.name = name
-  this.email = email
-  this.phone = phone
-  this.address = address
-  this.printInfo = function () {
-    console.log(`
-      ${this.name}
-      ${this.email}
-      ${this.phone}
-      ${this.address}
-    `)
-  }
-}
+// function User (name, email, phone, address) {
+//   this.name = name
+//   this.email = email
+//   this.phone = phone
+//   this.address = address
+//   this.printInfo = function () {
+//     console.log(`
+//       ${this.name}
+//       ${this.email}
+//       ${this.phone}
+//       ${this.address}
+//     `)
+//   }
+// }
 
-let user = new User('John Doe', 'johndoe@gmail.com', 1234567890, '123 Main St')
+// let user = new User('John Doe', 'johndoe@gmail.com', 1234567890, '123 Main St')
 
-user.printInfo()
+// user.printInfo()
 
+const User = (name, email, phone, address) => ({ name, email, phone, address })
 
+let user = User('John Doe', 'johndoe@gmail.com', 1234567890, '123 Main St')
+let user2 = User('Jane Doe', 'janedoe@gmail.com', 0987654321, '321 South St')
+
+console.log(user)
